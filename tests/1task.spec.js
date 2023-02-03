23ddb6248edc726de2d9a7eda445b6f977b5b337
Laura Mingilate <laura.mingilaite@juvare.com>
2023-02-03 10:25:27 +0200

    Tasks codes

--- tests/1task.spec.js
+++ tests/1task.spec.js
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   await page.goto('https://mikeleshtembari.github.io/jumping-frogs/');
+
   await page.locator('span:nth-child(3) > img').click();
   await page.locator('.pink > img').first().click();
   await page.locator('span:nth-child(6) > img').click();
